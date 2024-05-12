7c6a17893fb46ab0ef1c94a4186336732e0aeb26
Murilo <142051797+murilosantosb@users.noreply.github.com>
2024-05-12 20:59:42 -0300

    Update db.js

--- REACT_GRAM/backend/config/db.js
+++ REACT_GRAM/backend/config/db.js
@@ -5,7 +5,11 @@ const dbPassword = process.env.DB_PASS
 const conn = async () => {
     try{
         const dbConn = await mongoose.connect(
-            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.18gobjx.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://${dbUser}:${dbPassword}@cluster0.18gobjx.mongodb.net/?retryWrites=true&w=majority`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
         )
         console.log("Conectou ao banco!")
 
